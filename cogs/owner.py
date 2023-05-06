1cfd4a88ebe2e780aafc0fc3e2065b231ac9694e
Zyla <erencana12@gmail.com>
2023-05-06 23:33:34 +0300

    Update owner.py
    Add informative descriptions corresponding to each button.

--- cogs/owner.py
+++ cogs/owner.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+
 from utils.buttons import RoleMenuSetupButtons
 
 
@@ -13,8 +14,18 @@ class Owner(commands.Cog):
         embed = discord.Embed(
             color=discord.Color.purple(),
             title='Vanity Role Menu',
-            description='Here you can create or delete your vanity role.'
+            description='Here you can create/delete/edit your vanity role.'
         )
+        embed.add_field(name='Creating',
+                        value="You'll enter the role name, color in hexadecimal format and the icon url which is an "
+                              "image url.")
+        embed.add_field(name='Deleting',
+                        value="Nothing special here, bot will handle the work you just need to press the big red "
+                              "button.")
+        embed.add_field(name='Editing',
+                        value="For now, you don't have the option to leave blank or set to none, please fill in what "
+                              "your current role name/color/icon is if you don't want to change it.")
+        embed.set_footer(text='Only available for nitro boosters.', icon_url=ctx.me.avatar.url)
         await ctx.send(embed=embed, view=RoleMenuSetupButtons())
 
 
