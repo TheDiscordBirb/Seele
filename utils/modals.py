26e88fd1c5fe56d1433bb423070bb4960524f8ea
Zyla <erencana12@gmail.com>
2023-05-08 20:04:44 +0300

    Update modals.py

--- utils/modals.py
+++ utils/modals.py
@@ -7,61 +7,85 @@ from mongo import get_database
 
 
 class RoleMenuSetup(discord.ui.Modal):
-    name = discord.ui.TextInput(label='Role Name')
-    color = discord.ui.TextInput(label='Role Color (Hexadecimal)')
-    icon = discord.ui.TextInput(label='Role Icon')
+    name = discord.ui.TextInput(label="Role Name")
+    color = discord.ui.TextInput(label="Role Color (Hexadecimal w/o #)")
+    icon = discord.ui.TextInput(label="Role Icon", required=False)
 
     async def on_submit(self, interaction: discord.Interaction):
         db = get_database()
-        vanity_roles = db['Vanity Roles']
-        if vanity_roles.find_one({'_id': interaction.user.id}) is not None:
-            return await interaction.response.send_message('You already have a role set up!', ephemeral=True)
+        vanity_roles = db["Vanity Roles"]
+
+        if vanity_roles.find_one({"_id": interaction.user.id}):
+            return await interaction.response.send_message(
+                "You already have a role set up!", ephemeral=True
+            )
+
         parent_role = discord.utils.get(interaction.guild.roles, id=1101868829275066406)
-        r = requests.get(self.icon.value)
-        if r.status_code in range(200, 299):
-            img = BytesIO(r.content)
-            b = img.getvalue()
-        role = await interaction.guild.create_role(
-            name=self.name.value,
-            color=discord.Color.from_str(self.color.value),
-            display_icon=b
+        if self.icon.value.lower() == "":
+            role = await interaction.guild.create_role(
+                name=self.name.value,
+                color=discord.Color.from_str(f"#{self.color.value}"),
+                display_icon=None,
+            )
+            await role.edit(position=parent_role.position - 1)
+        else:
+            r = requests.get(self.icon.value)
+            if r.ok:
+                img = BytesIO(r.content)
+                b = img.getvalue()
+                role = await interaction.guild.create_role(
+                    name=self.name.value,
+                    color=discord.Color.from_str(f"#{self.color.value}"),
+                    display_icon=b,
+                )
+            await role.edit(position=parent_role.position - 1)
+
+        vanity_roles.insert_one(
+            {
+                "_id": interaction.user.id,
+                "role_name": self.name.value,
+                "role_id": role.id,
+            }
         )
-        await role.edit(position=parent_role.position - 1)
-        vanity_roles.insert_one({
-            '_id': interaction.user.id,
-            'name': self.name.value,
-            'role_id': role.id
-        })
+
         await interaction.user.add_roles(role)
-        await interaction.response.send_message('Role set up successfully!', ephemeral=True)
+        await interaction.response.send_message(
+            "Role set up successfully!", ephemeral=True
+        )
 
     async def on_error(self, interaction: discord.Interaction, error: Exception):
-        await interaction.response.send_message(f'Error: {error}', ephemeral=True)
+        await interaction.response.send_message(f"Error: {error}", ephemeral=True)
 
 
 class EditRoleModal(discord.ui.Modal):
-    name = discord.ui.TextInput(label='Role Name')
-    color = discord.ui.TextInput(label='Role Color (Hexadecimal)')
-    icon = discord.ui.TextInput(label='Role Icon')
+    name = discord.ui.TextInput(label="Role Name")
+    color = discord.ui.TextInput(label="Role Color (Hexadecimal w/o #)")
+    icon = discord.ui.TextInput(label="Role Icon", required=False)
 
     async def on_submit(self, interaction: discord.Interaction):
         db = get_database()
-        vanity_roles = db['Vanity Roles']
-        clr = discord.Color.from_str(self.color.value)
+        vanity_roles = db["Vanity Roles"]
+        clr = discord.Color.from_str(f"#{self.color.value}")
 
-        member_entry = vanity_roles.find_one({'_id': interaction.user.id})
-        role = discord.utils.get(interaction.guild.roles, id=member_entry['role_id'])
-
-        r = requests.get(self.icon.value)
-        if r.status_code in range(200, 299):
-            img = BytesIO(r.content)
-            b = img.getvalue()
-        await role.edit(
-            name=self.name.value,
-            color=clr,
-            display_icon=b
-        )
-        await interaction.response.send_message('Role edited successfully!', ephemeral=True)
+        player = vanity_roles.find_one({"_id": interaction.user.id})
+        role_id = player.get("role_id")
+        if role_id:
+            role = discord.utils.get(interaction.guild.roles, id=role_id)
+            img = None
+            if self.icon.value == "":
+                role.edit(name=self.name.value, color=clr)
+            else:
+                r = requests.get(self.icon.value)
+                if r.status_code in range(200, 299):
+                    img = BytesIO(r.content)
+                await role.edit(name=self.name.value, color=clr)
+                if img:
+                    await role.edit(icon=img.getvalue())
+            await interaction.response.send_message(
+                "Role edited successfully!", ephemeral=True
+            )
+        else:
+            await interaction.response.send_message("You don't have a vanity role!")
 
     async def on_error(self, interaction: discord.Interaction, error: Exception):
-        await interaction.response.send_message(f'Error: {error}', ephemeral=True)
+        await interaction.response.send_message(f"Error: {error}", ephemeral=True)
