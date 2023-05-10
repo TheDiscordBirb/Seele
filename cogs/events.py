5837193761cbf9419728dccc3c2af66ad85b0f53
Zyla <erencana12@gmail.com>
2023-05-10 21:52:46 +0300

    rename shields.py -> economy.py, update economy.py, reject slash commands return to prefix commands, disable xp gain by message

--- cogs/events.py
+++ cogs/events.py
@@ -1,8 +1,10 @@
+import datetime
 import sys
 import traceback
 
 import discord
 from discord.ext import commands
+import humanize
 
 from mongo import get_database
 
@@ -14,21 +16,19 @@ class Events(commands.Cog):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_command_error(self, ctx: commands.Context, error):
+    async def on_command_error(
+        self, ctx: commands.Context, error: commands.CommandError
+    ):
         try:
             raise error
+        except commands.CommandNotFound:
+            return
+        except commands.CommandOnCooldown:
+            return
         except commands.NoPrivateMessage:
             await ctx.author.send("This command cannot be used in DMs.")
         except commands.DisabledCommand:
             await ctx.send("This command is disabled.")
-        except commands.CommandNotFound:
-            return
-        except commands.CommandOnCooldown as e:
-            await ctx.send(
-                f"This command is on cooldown for {e.retry_after:.2f} seconds."
-            )
-        except commands.MissingPermissions:
-            await ctx.send("You do not have permission to use this command.")
         except commands.NotOwner:
             await ctx.send("This command is owner only.")
         except Exception as e:
@@ -41,47 +41,47 @@ class Events(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
-        db = get_database()
-        shields = db["Shields"]
-        if (
-            message.author.bot
-            or message.author == self.bot.user
-            or message.channel.id
-            in [
-                1103879081273073674,
-                1103879350719356948,
-                1105189907166662686,
-                1102599564705411142,
-            ]
-            or message.guild.get_role(1102951068599590912) not in message.author.roles
-        ):
-            return
+        return  # todo Enable on launch.
+        # db = get_database()
+        # shields = db["Shields"]
+        # if (
+        #     message.author.bot
+        #     or message.author == self.bot.user
+        #     or message.channel.id
+        #     in [
+        #         1103879081273073674,
+        #         1103879350719356948,
+        #         1105189907166662686,
+        #         1102599564705411142,
+        #     ]
+        # ):
+        #     return
 
-        if message.author.id in self.cooldowns:
-            remaining_time = (
-                self.cooldowns[message.author.id] - message.created_at.timestamp()
-            )
-            if remaining_time > 0:
-                return
-        self.cooldowns[message.author.id] = message.created_at.timestamp() + 20
-        player = shields.find_one({"_id": message.author.id})
-        if player is not None:
-            member = message.guild.get_member(message.author.id)
-            nitro_role = message.guild.get_role(1102939433038254091)
-            if nitro_role in member.roles:
-                shields.find_one_and_update(
-                    {"_id": message.author.id},
-                    {"$inc": {"Shields": 8}},
-                )
-            else:
-                shields.find_one_and_update(
-                    {"_id": message.author.id},
-                    {"$inc": {"Shields": 5}},
-                )
-        else:
-            shields.insert_one(
-                {"_id": message.author.id, "Shields": 5, "inventory": []}
-            )
+        # if message.author.id in self.cooldowns:
+        #     remaining_time = (
+        #         self.cooldowns[message.author.id] - message.created_at.timestamp()
+        #     )
+        #     if remaining_time > 0:
+        #         return
+        # self.cooldowns[message.author.id] = message.created_at.timestamp() + 20
+        # player = shields.find_one({"_id": message.author.id})
+        # if player is not None:
+        #     member = message.guild.get_member(message.author.id)
+        #     nitro_role = message.guild.get_role(1102939433038254091)
+        #     if nitro_role in member.roles:
+        #         shields.find_one_and_update(
+        #             {"_id": message.author.id},
+        #             {"$inc": {"Shields": 8}},
+        #         )
+        #     else:
+        #         shields.find_one_and_update(
+        #             {"_id": message.author.id},
+        #             {"$inc": {"Shields": 5}},
+        #         )
+        # else:
+        #     shields.insert_one(
+        #         {"_id": message.author.id, "Shields": 5, "inventory": []}
+        #     )
 
 
 async def setup(self: commands.Bot):
--- cogs/moderation.py
+++ cogs/moderation.py
@@ -7,92 +7,69 @@ class Moderation(commands.Cog):
         super().__init__()
         self.bot = bot
 
-    @discord.app_commands.command(
-        name="nsfw-ban", description="NSFW Ban the given member."
-    )
-    @discord.app_commands.describe(member="Member to ban.", reason="Ban reason.")
-    @discord.app_commands.checks.has_any_role(1101868829317013647, 1101868829296054320)
+    @commands.command(name="nsfwban", aliases=["nban"])
+    @commands.has_any_role(1101868829317013647, 1101868829296054320)
     async def nsfw_ban(
         self,
-        interaction: discord.Interaction,
+        ctx: commands.Context,
         member: discord.Member,
         reason: str = None,
     ):
-        member = interaction.guild.get_member(member.id)
+        member = ctx.guild.get_member(member.id)
         ban_reason = reason if reason is not None else "Reason not provided."
-        nsfw_banned_role = interaction.guild.get_role(1105208057417445386)
-        nsfw_access_role = interaction.guild.get_role(1104145533934772244)
+        nsfw_banned_role = ctx.guild.get_role(1105208057417445386)
+        nsfw_access_role = ctx.guild.get_role(1104145533934772244)
         if not member:
-            return await interaction.response.send_message(
-                "Invalid member.", ephemeral=True
-            )
+            return await ctx.send("Invalid member.")
         if nsfw_banned_role in member.roles:
-            return await interaction.response.send_message(
-                f"{member.mention} is already NSFW Banned."
-            )
+            return await ctx.send(f"{member.mention} is already NSFW Banned.")
         await member.remove_roles(nsfw_access_role)
         await member.add_roles(nsfw_banned_role)
         await member.send(
-            f"You've been NSFW Banned in **{interaction.guild.name}**\nReason: {ban_reason}"
+            f"You've been NSFW Banned in **{ctx.guild.name}**\nReason: {ban_reason}"
         )
-        await interaction.response.send_message(f"NSFW Banned {member.mention}\n")
+        await ctx.send(f"NSFW Banned {member.mention}\n")
 
     @nsfw_ban.error
-    async def nsfw_ban_error(self, interaction: discord.Interaction, error):
-        if isinstance(error, discord.app_commands.errors.MissingAnyRole):
-            return await interaction.response.send_message(error)
-        await interaction.response.send_message("An error occurred! Please try again.")
+    async def nsfw_ban_error(self, ctx: commands.Context, error):
+        if isinstance(error, commands.errors.MissingAnyRole):
+            return await ctx.response.send_message(error)
+        await ctx.send("An error occurred! Please try again.")
         print(error)
 
-    @discord.app_commands.command(
-        name="nsfw-unban", description="NSFW Unban the given member."
-    )
-    @discord.app_commands.describe(member="Member to unban.")
-    @discord.app_commands.checks.has_any_role(1101868829317013647, 1101868829296054320)
-    async def nsfw_unban(
-        self, interaction: discord.Interaction, member: discord.Member
-    ):
-        member = interaction.guild.get_member(member.id)
-        nsfw_banned_role = interaction.guild.get_role(1105208057417445386)
-        nsfw_access_role = interaction.guild.get_role(1104145533934772244)
+    @commands.command(name="nsfwunban", aliases=["nunban"])
+    @commands.has_any_role(1101868829317013647, 1101868829296054320)
+    async def nsfw_unban(self, ctx: commands.Context, member: discord.Member):
+        member = ctx.guild.get_member(member.id)
+        nsfw_banned_role = ctx.guild.get_role(1105208057417445386)
+        nsfw_access_role = ctx.guild.get_role(1104145533934772244)
         if not member:
-            return await interaction.response.send_message(
-                "Invalid member.", ephemeral=True
-            )
+            return await ctx.send("Invalid member.")
         if nsfw_banned_role not in member.roles:
-            return await interaction.response.send_message(
-                f"{member.mention} is not NSFW Banned."
-            )
+            return await ctx.send(f"{member.mention} is not NSFW Banned.")
         await member.add_roles(nsfw_access_role)
         await member.remove_roles(nsfw_banned_role)
-        await interaction.response.send_message(f"NSFW Unbanned {member.mention}")
+        await ctx.send(f"NSFW Unbanned {member.mention}")
 
-    @discord.app_commands.command(
-        name="say", description="Send a message to a channel."
-    )
-    @discord.app_commands.checks.has_any_role(1101868829317013647, 1101868829296054320)
-    @discord.app_commands.describe(
-        message="Message to send.",
-        channel="What channel do you want to send the message to?",
-    )
+    @commands.command(name="say")
+    @commands.has_any_role(1101868829317013647, 1101868829296054320)
     async def say(
         self,
-        interaction: discord.Interaction,
+        ctx: commands.Context,
         message: str,
         channel: discord.TextChannel = None,
     ):
         if channel is None:
-            await interaction.response.send_message("Sent", ephemeral=True)
-            return await interaction.channel.send(message)
-        await interaction.guild.get_channel(channel.id).send(message)
-        await interaction.response.send_message("Sent", ephemeral=True)
+            await ctx.send("Sent")
+            return await ctx.channel.send(message)
+        await ctx.guild.get_channel(channel.id).send(message)
+        await ctx.send("Sent")
 
     @say.error
-    async def say_error(self, interaction: discord.Interaction, error):
-        if isinstance(error, discord.app_commands.errors.MissingAnyRole):
-            return await interaction.response.send_message(error)
-        await interaction.response.send_message("An error occurred! Please try again.")
-        print(error)
+    async def say_error(self, ctx: commands.Context, error):
+        if isinstance(error, commands.errors.MissingAnyRole):
+            return await ctx.send(error)
+        await ctx.send("An error occurred! Please try again.")
 
 
 async def setup(self: commands.Bot):
