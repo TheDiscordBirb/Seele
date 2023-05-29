2be7d7ca7cd7dc27ab85f55205c3b3e4c0c24a7b
Birb <bluevidray.stream@gmail.com>
2023-05-29 22:49:43 +0200

    Update owner.py

--- cogs/owner.py
+++ cogs/owner.py
@@ -55,7 +55,7 @@ class Owner(commands.Cog):
     @commands.guild_only()
     async def give(self, ctx: commands.Context):
         role = discord.utils.get(ctx.guild.roles, id=1101868829317013647)
-        if role in ctx.author.roles() or ctx.author.id == 464417492060733440:
+        if role in ctx.author.roles or ctx.author.id == 464417492060733440:
             ctx.reply("Test successful!")
         #db = get_database()["Economy"]
         #db.update_one(
