3c252c7bfbecc8bc01689824990d6c26226e3f74
Eilyz <erencana12@gmail.com>
2023-05-30 01:01:06 +0300

    refactor admin.py command give

--- cogs/admin.py
+++ cogs/admin.py
@@ -7,42 +7,41 @@ from mongo import get_database
 class Admin(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.admin_roleID = 1101868829317013647
-        
+
     @commands.command(name="give")
+    @commands.has_any_role(1101868829317013647, 1103789020456173630)
     @commands.guild_only()
-    async def give(self, ctx: commands.Context, member: discord.Member = None, amt: int = None):
+    async def give(
+        self, ctx: commands.Context, member: discord.Member = None, amt: int = None
+    ):
+        if member is None:
+            return await ctx.reply("Specify member.")
         if amt is None:
-            await ctx.reply(f"Specify amount")
-        else:
-            if member is None:
-                await ctx.reply(f"Specify user")
-            else:
-                role = discord.utils.get(ctx.guild.roles, id=1101868829317013647)
-                if role in ctx.author.roles or ctx.author.id in self.bot.owner_ids:
-                    db = get_database()["Economy"]
-                    db.update_one(
-                        {"_id": member.id},
-                        {"$inc":{"shields": amt}}
-                    )
-                    channel = ctx.guild.get_channel(1112849838812438619)
-                    if amt > 0:
-                        if amt == 1:
-                            await ctx.reply(f"`{ctx.author.name}` gave `{member.name}` `{amt}` shield")
-                            await channel.send(f"`{ctx.author.name}` gave `{member.name}` `{amt}` shield")
-                        else:
-                            await ctx.reply(f"`{ctx.author.name}` gave `{member.name}` `{amt}` shields")
-                            await channel.send(f"`{ctx.author.name}` gave `{member.name}` `{amt}` shields")
-                    elif amt < 0:
-                        if amt == -1:
-                            await ctx.reply(f"`{ctx.author.name}` took away `{amt*-1}` shield from `{member.name}`")
-                            await channel.send(f"`{ctx.author.name}` took away `{amt*-1}` shield from `{member.name}`")
-                        else:
-                            await ctx.reply(f"`{ctx.author.name}` took away `{amt*-1}` shields from `{member.name}`")
-                            await channel.send(f"`{ctx.author.name}` took away `{amt*-1}` shields from `{member.name}`")
-                    
-        
-        
-        
+            return await ctx.reply("Specify amount.")
+
+        shield_text = "shield" if abs(amt) == 1 else "shields"
+        shield_verb = "took away" if amt < 0 else "gave"
+        amt_abs = abs(amt)
+
+        role = discord.utils.get(ctx.guild.roles, id=1101868829317013647)
+        if role in ctx.author.roles or ctx.author.id in self.bot.owner_ids:
+            db = get_database()["Economy"]
+            db.update_one({"_id": member.id}, {"$inc": {"shields": amt}})
+
+        msg = (
+            f"`{ctx.author.name}` {shield_verb} `{amt_abs}` {shield_text}"
+            f" from/to `{member.name}`"
+        )
+
+        await ctx.send(msg)
+
+    @give.error
+    async def give_error(self, ctx: commands.Context, error: commands.CommandError):
+        if isinstance(error, commands.MissingRole):
+            return await ctx.reply("You don't have permission to use this command.")
+        if isinstance(error, commands.MemberNotFound):
+            return await ctx.reply("Invalid Member.")
+
+
 async def setup(self: commands.Bot):
-    await self.add_cog(Admin(self))     
+    await self.add_cog(Admin(self))
