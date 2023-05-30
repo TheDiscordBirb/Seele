d92f8885a0e50e9ef78c1801e7a4f8141f00de56
Birb <bluevidray.stream@gmail.com>
2023-05-30 08:33:35 +0200

    Update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -12,26 +12,34 @@ class Admin(commands.Cog):
     @commands.command(name="give")
     @commands.guild_only()
     async def give(self, ctx: commands.Context, member: discord.Member = None, amt: int = None):
+        
+        if member is None:
+            return await ctx.reply("Specify member.")
         if amt is None:
-            await ctx.reply(f"Specify amount")
-        else:
-            if member is None:
-                await ctx.reply(f"Specify user")
-            else:
-                role = discord.utils.get(ctx.guild.roles, id=1101868829317013647)
-                if role in ctx.author.roles or ctx.author.id in self.bot.owner_ids:
-                    db = get_database()["Economy"]
-                    db.update_one(
-                        {"_id": member.id},
-                        {"$inc":{"shields": amt}}
-                    )
-                    channel = ctx.guild.get_channel(1112849838812438619)
-                    if amt > 0:
-                        await ctx.reply(f"`{ctx.author.name}` gave `{member.name}` `{amt}` <:Shields_SM:1104809716460310549>")
-                        await channel.send(f"`{ctx.author.name}` gave `{member.name}` `{amt}` <:Shields_SM:1104809716460310549>")
-                    elif amt < 0:
-                        await ctx.reply(f"`{ctx.author.name}` took away `{amt*-1}` <:Shields_SM:1104809716460310549> from `{member.name}`")
-                        await channel.send(f"`{ctx.author.name}` took away `{amt*-1}` <:Shields_SM:1104809716460310549> from `{member.name}`")
+            return await ctx.reply("Specify amount.")
+        
+        role = discord.utils.get(ctx.guild.roles, id=1101868829317013647)
+        if role in ctx.author.roles or ctx.author.id in self.bot.owner_ids:
+            db = get_database()["Economy"]
+            db.update_one(
+                {"_id": member.id},
+                {"$inc":{"shields": amt}}
+            )
+            shield_text = "shield" if abs(amt) == 1 else "shields"
+            channel = ctx.guild.get_channel(1112849838812438619)
+            if amt > 0:
+                await ctx.reply(f"`{ctx.author.name}` gave `{member.name}` `{abs(amt)}` `{shield_text}`<:Shields_SM:1104809716460310549>")
+                await channel.send(f"`{ctx.author.name}` gave `{member.name}` `{abs(amt)}` `{shield_text}`<:Shields_SM:1104809716460310549>")
+            elif amt < 0:
+                await ctx.reply(f"`{ctx.author.name}` took away `{abs(amt)}` `{shield_text}`<:Shields_SM:1104809716460310549> from `{member.name}`")
+                await channel.send(f"`{ctx.author.name}` took away `{abs(amt)}` `{shield_text}`<:Shields_SM:1104809716460310549> from `{member.name}`")
+                        
+    @give.error
+    async def give_error(self, ctx: commands.Context, error: commands.CommandError):
+        if isinstance(error, commands.MissingRole):
+            return await ctx.reply("You don't have permission to use this command.")
+        if isinstance(error, commands.MemberNotFound):
+            return await ctx.reply("Invalid Member.")
                     
         
         
